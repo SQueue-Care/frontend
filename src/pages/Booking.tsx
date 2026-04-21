1626870a45b37d19b9e335466be65a40cfabf31d
BintangWishnu <wishnupay@gmail.com>
2026-04-22 00:49:20 +0700

    chore: save local gitignore and readme updates (#24)

---
+++ src/pages/Booking.tsx
@@ -0,0 +1,7 @@
+export default function Booking() {
+    return (
+        <div className="min-h-screen flex items-centerjustify-center bg-slate-50">
+            <h1 className="text-3xl font-bold text-teal-600">Ini Halaman Booking</h1>
+        </div>
+    )
+}
